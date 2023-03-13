07e723479d90944f9e1adf132480a5d507705030
aanddi <kuropatkinn.a.i21@gmail.com>
2023-03-13 22:27:50 +0300

    Валидация полей ввода

--- frontend/src/views/Auth/LoginView.vue
+++ frontend/src/views/Auth/LoginView.vue
@@ -3,13 +3,15 @@
       <v-row justify="center">
          <v-col cols="12" sm="8" lg="6">
             <v-card class="elevetion-12">
-               <v-toolbar dark color="primary" class="pl-4"> Login </v-toolbar>
+               <v-toolbar dark color="primary">
+                  <v-toolbar-title>Login</v-toolbar-title>
+               </v-toolbar>
                <v-card-text>
-                  <v-form>
-                     <v-text-field prepend-icon="mdi-account" name="email" label="Email" type="email"
-                        v-model="email"></v-text-field>
+                  <v-form v-model="valid" ref="form" validation>
+                     <v-text-field prepend-icon="mdi-account" name="email" label="Email" type="email" v-model="email"
+                        :rules="emailRules"></v-text-field>
                      <v-text-field prepend-icon="mdi-lock" name="password" label="Password" type="password"
-                        v-model="password"></v-text-field>
+                        v-model="password" :rules="passwordRules"></v-text-field>
                   </v-form>
                </v-card-text>
                <v-card-actions>
@@ -21,15 +23,24 @@
       </v-row>
    </v-container>
 </template>
- 
+
 <script>
+
 export default {
-   name: 'LoginView',
-   date() {
+   data() {
       return {
          email: "",
-         password: ""
+         password: "",
+         valid: false,
+         emailRules: [
+            v => !!v || 'E-mail is required',
+            v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+         ],
+         passwordRules: [
+            v => !!v || 'Name is required',
+            v => (v && v.length >= 6) || 'Password must be more or equel than 6 characters',
+         ]
       }
    }
-}
+} 
 </script>
