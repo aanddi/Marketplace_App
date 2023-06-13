bbc98ebf83cc1fc45763bdfe87955b1be9187b16
aanddi <kuropatkinn.a.i21@gmail.com>
2023-06-13 13:00:25 +0300

    Создание getter isUserLoggedIn

--- frontend/src/store/user.js
+++ frontend/src/store/user.js
@@ -8,71 +8,79 @@ class User {
 
 
 export default {
-	state: {
+    state: {
         user: null
     },
-	mutations: {
-        setUser(state,payload) {
-        console.log(payload)
-        state.user = payload
+    mutations: {
+        setUser(state, payload) {
+            console.log(payload)
+            state.user = payload
         }
     },
     actions: {
-        async registerUser({commit},{email, password}) {
-        commit('clearError')
-        commit('setLoading', true)
-        //Здесь выполняется запрос на сервер
-        let isRequestOk = false
-        let promise = new Promise(function(resolve) {
-        setTimeout(() => resolve('Done')
-        , 3000);
-    });
+        async registerUser({
+            commit
+        }, {
+            email,
+            password
+        }) {
+            commit('clearError')
+            commit('setLoading', true)
+            //Здесь выполняется запрос на сервер
+            let isRequestOk = false
+            let promise = new Promise(function (resolve) {
+                setTimeout(() => resolve('Done'), 3000);
+            });
 
 
-if (isRequestOk) {
-await promise.then(()=> {
-        commit('setUser', new User(1, email, password))
-        commit('setLoading', false)
-        })
-        } else {
-        await promise.then(()=> {
-        commit('setLoading', false)
-        commit('setError', 'Ошибка регистрации')
-        throw 'Упс... Ошибка регистрации'
-    })
-    }
-
-        loginUser({commit},{email, password}); {
-        commit('clearError')
-        commit('setLoading', true)
-        //Здесь выполняется запрос на сервер
-        let isRequestOk = false
-        let promise = new Promise(function(resolve) {
-        setTimeout(() => resolve('Done')
-        , 3000);
-    });
-
-
-    if (isRequestOk) {
-    await promise.then(()=> {
-        commit('setUser', new User(1, email, password))
-        commit('setLoading', false)
-        })
-        } else {
-        await promise.then(()=> {
-        commit('setLoading', false)
-        commit('setError', 'Ошибка логина или пароля')
-        throw 'Упс... Ошибка логина или пароля'
-    })
-    }
-    }
+            if (isRequestOk) {
+                await promise.then(() => {
+                    commit('setUser', new User(1, email, password))
+                    commit('setLoading', false)
+                })
+            } else {
+                await promise.then(() => {
+                    commit('setLoading', false)
+                    commit('setError', 'Ошибка регистрации')
+                    throw 'Упс... Ошибка регистрации'
+                })
+            }
+        },
+        async loginUser({
+            commit
+        }, {
+            email,
+            password
+        }) {
+            commit('clearError')
+            commit('setLoading', true)
+            //Здесь выполняется запрос на сервер
+            let isRequestOk = false
+            let promise = new Promise(function (resolve) {
+                setTimeout(() => resolve('Done'), 3000);
+            });
 
 
-},
-	getters: {
+            if (isRequestOk) {
+                await promise.then(() => {
+                    commit('setUser', new User(1, email, password))
+                    commit('setLoading', false)
+                })
+            } else {
+                await promise.then(() => {
+                    commit('setLoading', false)
+                    commit('setError', 'Ошибка логина или пароля')
+                    throw 'Упс... Ошибка логина или пароля'
+                })
+            }
+        }
+    },
+    getters: {
         user(state) {
             return state.user
-	}
-}
-}
+        },
+        isUserLoggedIn (state) {
+	        return state.user !== null
+        }
+    }
 }
