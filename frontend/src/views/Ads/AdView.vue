2b33c130bba439ab0338ec4ebe123b595886003f
aanddi <kuropatkinn.a.i21@gmail.com>
2023-06-13 13:19:03 +0300

    Подключение диалогового окна

--- frontend/src/views/Ads/AdView.vue
+++ frontend/src/views/Ads/AdView.vue
@@ -1,31 +1,38 @@
-<template> 
+<template>
 	<v-container>
-		<v-layout row> 
-			<v-flex xs12>
-				<v-card class="mt-5">
-					<v-img :src="ad.src"></v-img>
-					<v-card-text>
+	    <v-row>
+		   <v-col cols="12">
+			  <v-card class="mt-5">
+				 <v-img height="300px" :src="ad.src"></v-img>
+				 <v-card-text>
 					<h1 class="text--primary mb-3">{{ ad.title }}</h1>
-					<p>{{ ad.desc }}</p>
-					</v-card-text>
-					<v-card-actions>
+					<p>
+					    {{ ad.desc }}
+					</p>
+				 </v-card-text>
+				 <v-card-actions>
 					<v-spacer></v-spacer>
-					<v-btn text class="warning">Edit</v-btn>
-					<v-btn class="success">Buy</v-btn>
-					</v-card-actions>	
-				</v-card>
-			</v-flex>
-		</v-layout> 
+					<modal-dialog></modal-dialog>
+					<v-btn class="success" color="green">Buy</v-btn>
+				 </v-card-actions>
+			  </v-card>
+		   </v-col>
+	    </v-row>
 	</v-container>
-</template>
-<script>
-export default {
+ </template>
+ <script>
+ import EditAdModal from '../EditAdModal'
+ export default {
 	props: ['id'],
 	computed: {
-		ad() {
-		const id = this.id
-		return this.$store.getters.adById(id)
-		}
+	    ad() {
+		   const id = this.id
+		   return this.$store.getters.adById(id)
+	    }
+	},
+	components: {
+	    'modal-dialog': EditAdModal
 	}
-} 
-</script>
+ 
+ };
+ </script>
